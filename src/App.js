e060be6cec6d86ac324c2a5e643f8fe86afa692f
MarkusZP <markslavenkoo@ukr.net>
2018-10-16 13:53:40 +0300

    half-working project

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css'
 import {Shows} from "./components/Shows/Shows";
 import shortid from 'shortid'
 import { connect } from 'react-redux'
-import PropTypes from 'prop-types';
-import { pageChange } from './actions/pageChange'
-import { searchByValue } from './actions/searchByValue'
-import { clearPage } from './actions/clearPage'
+// import PropTypes from 'prop-types';
+// import { pageChange } from './actions/pageChange'
+// import { searchByValue } from './actions/searchByValue'
+// import { clearPage } from './actions/clearPage'
+import {
+    urlMaker,
+    setQuery,
+    changePage
+} from './actions/index.js'
 
 class App extends Component{
+
+
 
     constructor(props) {
         super(props)
@@ -30,8 +37,12 @@ class App extends Component{
         this.setState({visibleBtn:false})
     }
 
+    componentDidMount() {
+        this.props.urlMaker('https://api.trakt.tv/shows/popular?extended=full&limit=10&genres=action', 'Action');
+    }
 
     render() {
+        console.log(this.props)
         const shows_pagination = this.props.shows_pagination
         const sort = this.props.sort
         const page = this.props.page
@@ -78,20 +89,19 @@ class App extends Component{
                         {showsTemplate}
                         </tbody>
                     </table>
-                    <button onClick={i>0 ? ()=>page('PREV') : undefined} className={'prev btn ' + (i>0 ? '' : 'disabled')}><span>&larr;</span></button>
-                    <button onClick={i<=this.props.shows.length-30 ? ()=>page('NEXT') : undefined} className={'next btn ' + (i>=this.props.shows.length-30 ? 'disabled' : '')}><span>&rarr;</span></button>
-                    <p className="total">Number of TV shows: <span>{this.props.shows.length}</span></p>
+                    <button className={'prev btn '}><span>&larr;</span></button>
+                    <button  className={'next btn '}><span>&rarr;</span></button>
+                    {/*<p className="total">Number of TV shows: <span>{this.props.shows.length}</span></p>*/}
                 </div>
             </div>
 )}
 }
 
-App.propTypes = {
-    shows: PropTypes.array.isRequired
-}
+// App.propTypes = {
+//     shows: PropTypes.array.isRequired
+// }
 
 const mapStateToProps = store => {
-    console.log(store)
     return {
         page: store.other.page,
         loading: store.other.loading,
@@ -103,11 +113,19 @@ const mapStateToProps = store => {
     }
 }
 
-const mapDispatchToProps = dispatch => ({
-    page: Type => dispatch(pageChange(Type)),
-    search: Phrase => dispatch(searchByValue(Phrase)),
-    clear: () => dispatch(clearPage())
-})
+function mapDispatchToProps (dispatch) {
+    return ({
+        urlMaker: (url, category) => {
+            dispatch(urlMaker(url, category))
+        },
+        onChangeQuery: (query) => {
+            dispatch(setQuery(query))
+        },
+        onChangePage: (page) => {
+            dispatch(changePage(page))
+        }
+    })
+}
 
 export default connect(
     mapStateToProps,
--- src/actions/index.js
+++ src/actions/index.js
@@ -11,17 +11,24 @@ import {
     SET_SORT
 } from '../constants/index.js';
 
+export const setPage = page => {
+    return({
+        type: CHANGE_PAGE,
+        page
+    })
+}
+
+export const setMaxPage = maxPage => {
+    return ({
+        type: SET_MAX_PAGE,
+        maxPage
+    })
+}
+
 export const isLoading = loading => {
     return ({
         type: SET_LOADING,
         loading
-    })
-}
-
-export const setNewContent = content => {
-    return({
-        type: SET_NEW_CONTENT,
-        content
     })
 }
 
@@ -32,15 +39,50 @@ export const nothingFound = nothingFound => {
     })
 }
 
+export const setNewContent = content => {
+    return({
+        type: SET_NEW_CONTENT,
+        content
+    })
+}
+
+export const setUrl = url => {
+    return({
+        type: SET_URL,
+        url
+    })
+}
+
+export const setCategory = category => {
+    return({
+        type: SET_CATEGORY,
+        category
+    })
+}
+
+export const setQuery = query => {
+    return({
+        type: SET_QUERY,
+        query
+    })
+}
+
+export const setSort = sort => {
+    return ({
+        type: SET_SORT,
+        sort
+    })
+}
+
 export const loadContent = (url) => {
     return (dispatch, getState) => {
         dispatch(isLoading(true));
-        if (getState().otherContent.nothingFound) dispatch(nothingFound(false))
+        // if (getState().otherContent.nothingFound) dispatch(nothingFound(false))
         fetch(url, {
             headers: {
                 'Content-Type': 'application/json',
                 'trakt-api-version': '2',
-                'trakt-api-key': '50bdd2eaaf44ded248ade12837ff8a7a77e14add02efb9b0ef7edc034dab134a',
+                'trakt-api-key': '24121afa0914eaddbfa752b63590b6021628bc7c3bbc374a4116963ccadefcdf',
             },
         })
             .then((response) => {
@@ -62,5 +104,98 @@ export const loadContent = (url) => {
                     return newArrContent;
                 });
             })
+            .then((data) => { // загрузка картинок
+                const content = [...data];
+                let i = 0;
+                data.map((item, index) => {
+                    fetch(`https://webservice.fanart.tv/v3/tv/${item.ids.tvdb}?api_key=408b22edfb0465364b031d789388adb3`)
+                        .then((image) => {
+                            image.json().then((imageUrl) => {
+                                const newItem = Object.assign({}, item);
+                                if (imageUrl.tvposter) { // Перебор большинства возможных значений нахождения Url картинки в JSON объекте
+                                    newItem.url = imageUrl.tvposter[0].url;
+                                } else if (imageUrl.clearlogo) {
+                                    newItem.url = imageUrl.clearlogo[0].url;
+                                } else if (imageUrl.hdtvlogo) {
+                                    newItem.url = imageUrl.hdtvlogo[0].url;
+                                } else if (imageUrl.status === 'error') {
+                                    newItem.url = 'https://vignette.wikia.nocookie.net/citrus/images/6/60/No_Image_Available.png/revision/latest?cb=20170129011325';
+                                } else {
+                                    newItem.url = 'https://cs.pikabu.ru/post_img/big/2013/11/22/5/1385100645_1399473065.jpg';
+                                }
+                                content.splice(index, 1, newItem);
+                                i++;
+                                if (i === content.length){ // почему если обновлять много раз, то перерендерить реакт только 1 раз может (первый) и если убрать этот if то отобразиться только одна картинка
+                                    dispatch(setNewContent(content))
+                                    console.log(content)
+                                    dispatch(isLoading(false));
+                                }
+                            });
+                        });
+                });
+            });
+    }
+}
+
+export const urlMaker = (url, category) => {
+    return (dispatch) => {
+        dispatch(setQuery(''))
+        dispatch(setCategory(category));
+        dispatch(setPage(1));
+        dispatch(setUrl(url));
+        const fullUrl = `${url}&page=1`;
+        console.log(fullUrl);
+        dispatch(loadContent(fullUrl));
+    }
+}
+
+export const changePage = (page) => {
+    return (dispatch, getState) => {
+        dispatch(setPage(page));
+        console.log(`${getState().otherContent.url}&page=${page}`);
+        dispatch(loadContent(`${getState().otherContent.url}&page=${page}`))
+
+    }
+}
+
+export const sortFun = (selector) => {
+    return (dispatch, getState) => {
+        const newContent = [...getState().mainContent.content];
+        let newSort = {};
+        if (getState().otherContent.sort.direction === 'up' && selector === getState().otherContent.sort.sortOn) {
+            newSort = {
+                sortOn: selector,
+                direction: 'down',
+            };
+            if (selector === 'year') {
+                newContent.sort((a, b) => {
+                    if (a.year > b.year) return 1;
+                    if (a.year < b.year) return -1;
+                });
+            } else if (selector === 'rating') {
+                newContent.sort((a, b) => {
+                    if (a.rating > b.rating) return 1;
+                    if (a.rating < b.rating) return -1;
+                });
+            }
+        } else {
+            if (selector === 'year') {
+                newContent.sort((a, b) => {
+                    if (a.year > b.year) return -1;
+                    if (a.year < b.year) return 1;
+                });
+            } else if (selector === 'rating') {
+                newContent.sort((a, b) => {
+                    if (a.rating > b.rating) return -1;
+                    if (a.rating < b.rating) return 1;
+                });
+            }
+            newSort = {
+                sortOn: selector,
+                direction: 'up',
+            };
+        }
+        dispatch(setSort(newSort));
+        dispatch(setNewContent(newContent));
     }
 }
